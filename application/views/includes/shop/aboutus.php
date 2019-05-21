c5e9e3a0faa03aacdb14742bc4c95d4322679e0c
azurast <38053935+azurast@users.noreply.github.com>
2019-05-21 12:27:13 +0700

    update view  (#25)
    * ilangin foto customer di tabel customer admin
    
    * delete foto diganti jd placeholder di profil user
    
    * foto about us

--- application/views/includes/shop/aboutus.php
+++ application/views/includes/shop/aboutus.php
@@ -3,37 +3,37 @@
         <h2 class="text-success">Our Front End Team</h2>
         <div class="row" id="fontEnd">
           <div class="col-sm-4 col-6 mt-4 mt-sm-0">
-                <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                <img src="<?php echo base_url('argon\assets\img\steve.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
                 <small class="d-block text-uppercase text-white font-weight-bold">Steve Manumpil</small>
-                <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+                <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 21768</small>
           </div>
           <div class="col-sm-4 col-6 mt-4 mt-sm-0">
-                <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                <img src="<?php echo base_url('argon\assets\img\azura.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
                 <small class="d-block text-uppercase text-white font-weight-bold">Azura Sakan Taufik</small>
                 <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 20528</small>
           </div>
           <div class="col-sm-4 col-6 mt-4 mt-sm-0">
-                <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                <img src="<?php echo base_url('argon\assets\img\fanny.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
                 <small class="d-block text-uppercase text-white font-weight-bold">Cyntia Fanny</small>
-                <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+                <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 20976</small>
           </div>
         </div>
         <h2 class="text-warning">Our Back End Team</h2>
         <div class="row" id="backEnd">
             <div class="col-sm-4 col-6 mt-4 mt-sm-0">
-                  <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                  <img src="<?php echo base_url('argon\assets\img\cimi.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
                   <small class="d-block text-uppercase text-white font-weight-bold">Cindy Michelle</small>
-                  <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+                  <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 16358</small>
             </div>
             <div class="col-sm-4 col-6 mt-4 mt-sm-0">
-                  <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                  <img src="<?php echo base_url('argon\assets\img\sam.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
                   <small class="d-block text-uppercase text-white font-weight-bold">Samuel Lorent</small>
-                  <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+                  <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 21863</small>
             </div>
             <div class="col-sm-4 col-6 mt-4 mt-sm-0">
-                  <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                  <img src="<?php echo base_url('argon\assets\img\kiki.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
                   <small class="d-block text-uppercase text-white font-weight-bold">Handriki Kasa</small>
-                  <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+                  <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 14261</small>
             </div>
         </div>
 
